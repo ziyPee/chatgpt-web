6074eaf355d457444c6b5a72a5f8b83430735d30
ziyPee <zhiyong.pi@hand-china.com>
2023-09-01 11:32:52 +0800

    feat: add customer style

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -13,14 +13,14 @@ export interface UserState {
 }
 
 export function defaultSetting(): UserState {
-  const emptyGIF = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
+  // const emptyGIF = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs='
   return {
     userInfo: {
       // avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
       // name: 'ChenZhaoYu',
       // description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
-      avatar: import.meta.env.VITE_WEB_AVATAR || emptyGIF,
-      name: import.meta.env.VITE_WEB_NAME || 'Anonymous',
+      avatar: 'https://raw.githubusercontent.com/ziyPee/chatgpt-web/main/src/assets/avatar.jpg',
+      name: 'ZiyPee',
       description: 'Star on <a href="https://github.com/ziyPee/chatgpt-web" class="text-blue-500" target="_blank">Github</a>',
     },
   }
